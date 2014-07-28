21e8c6bcb467ebe8be1cbbfae186b11309ca0057
Manuela Schrenk <msch@kupferwerk.net>
2014-07-29 00:55:41 +0200

    new detail view for konfi

---
+++ WhatsKonfi/WhatsKonfi/WKKonfiDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKKonfiDetailViewController.h
+//  WhatsKonfi
+//
+//  Created by Manuela Schrenk on 29/07/14.
+//  Copyright (c) 2014 BitRoyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKKonfiDetailViewController : UIViewController
+
+@end
---
+++ WhatsKonfi/WhatsKonfi/WKKonfiDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  WKKonfiDetailViewController.m
+//  WhatsKonfi
+//
+//  Created by Manuela Schrenk on 29/07/14.
+//  Copyright (c) 2014 BitRoyal. All rights reserved.
+//
+
+#import "WKKonfiDetailViewController.h"
+
+@interface WKKonfiDetailViewController ()
+
+@end
+
+@implementation WKKonfiDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
